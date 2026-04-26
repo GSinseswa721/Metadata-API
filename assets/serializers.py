79f6c40e4edac98f40d43f85b106832cd5a3a481
GSinseswa721 <glorianiyonkurusinseswa@gmail.com>
2026-04-26 18:53:47 +0200

    feat: add serializers for Asset, Tag and ChangeLog

---
+++ assets/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from django.utils.text import slugify
+from .models import Asset, Tag, ChangeLog
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = Tag
+        fields = ['id', 'name', 'slug']
+        read_only_fields = ['slug']
+
+    def create(self, validated_data):
+        validated_data['slug'] = slugify(validated_data['name'])
+        return super().create(validated_data)
+
+
+class ChangeLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = ChangeLog
+        fields = ['id', 'change_summary', 'snapshot', 'changed_at']
+
+
+class AssetSerializer(serializers.ModelSerializer):
+    tags    = TagSerializer(many=True, read_only=True)
+    tag_ids = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Tag.objects.all(),
+        write_only=True, source='tags', required=False
+    )
+
+    class Meta:
+        model  = Asset
+        fields = [
+            'id', 'title', 'description', 'asset_type',
+            'file_url', 'status', 'tags', 'tag_ids',
+            'created_at', 'updated_at'
+        ]
+        read_only_fields = ['created_at', 'updated_at']
